54cc44b0cecfacdd706b5a82aa9878c8ea0ea396
124tranvita <124tranvita@gmail.com>
2022-04-28 00:28:15 +0700

    Basic AddStaffComponent: DONE

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -1,25 +1,37 @@
-import { useState } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
-function AddStaff() {
+function AddStaff({ currentStaffList }) {
+  const storageStaffs = JSON.parse(localStorage.getItem("staffs"));
+  console.log(storageStaffs);
+
   const [show, setShow] = useState(false);
-  const [staffs, setStaffs] = useState([]);
-  const [staff, setStaff] = useState(
-    {
-      name: '',
-      doB: '',
-      salaryScale: '',
-      startDate: '',
-      department: '',
-      annualLeave: '',
-      overTime: '',
-      salary: '',
-      image: '/assets/images/employee.svg',
-    }
-  )
+  const [staffs, setStaffs] = useState(storageStaffs ?? []);
+  const [staffId, setStaffId] = useState(
+    storageStaffs === null
+      ? currentStaffList.length
+      : currentStaffList.length + storageStaffs.length
+  );
 
+  console.log(staffId);
+
+  const [staff, setStaff] = useState({
+    id: 0,
+    name: "",
+    doB: "",
+    salaryScale: "",
+    startDate: "",
+    department: "",
+    annualLeave: "",
+    overTime: "",
+    image: "/assets/images/employee.svg",
+  });
+
+  // Show/Close Modal handler
   const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+  };
 
   const handleInputChange = (event) => {
     const target = event.target;
@@ -28,29 +40,30 @@ function AddStaff() {
 
     setStaff({
       ...staff,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (event) => {
-    console.log("Current State is: " + JSON.stringify(staff));
-    alert("Current State is: " + JSON.stringify(staff));
+    //console.log("Current State is: " + JSON.stringify(staff));
+    //alert("Current State is: " + JSON.stringify(staff));
 
-    setStaffs(prevState => {
-      const newStaffs = [...prevState, staff];
+    setStaffs((prevState) => {
+      const newStaffs = [...prevState, { ...staff, id: staffId }];
 
-      localStorage.setItem('staffs', JSON.stringify(newStaffs))
+      localStorage.setItem("staffs", JSON.stringify(newStaffs));
 
       return newStaffs;
-    })
+    });
 
-
-    event.preventDefault();
-  }
+    //event.preventDefault();
+  };
 
   return (
     <>
-      <Button variant='primary' onClick={handleShow}>Add Staff</Button>
+      <Button variant="primary" onClick={handleShow}>
+        Add Staff
+      </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -58,21 +71,38 @@ function AddStaff() {
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={(event) => handleSubmit(event)}>
-            <Form.Group className="mb-3" controlId='name'>
+            <Form.Group className="mb-3" controlId="name">
               <Form.Label>Tên</Form.Label>
-              <Form.Control type="text" name="name" placeholder='Họ và Tên' onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Họ và Tên"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId='doB'>
+            <Form.Group className="mb-3" controlId="doB">
               <Form.Label>Ngày sinh</Form.Label>
-              <Form.Control type="date" name="doB" onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="date"
+                name="doB"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId='startDate'>
+            <Form.Group className="mb-3" controlId="startDate">
               <Form.Label>Ngày vào công ty</Form.Label>
-              <Form.Control type="date" name="startDate" onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="date"
+                name="startDate"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId='department'>
+            <Form.Group className="mb-3" controlId="department">
               <Form.Label>Phòng ban</Form.Label>
-              <Form.Select aria-label="Department Select" name="department" onChange={(event) => handleInputChange(event)}>
+              <Form.Select
+                aria-label="Department Select"
+                name="department"
+                onChange={(event) => handleInputChange(event)}
+              >
                 <option value="1">Sale</option>
                 <option value="2">HR</option>
                 <option value="3">Marketing</option>
@@ -80,26 +110,38 @@ function AddStaff() {
                 <option value="5">Finance</option>
               </Form.Select>
             </Form.Group>
-            <Form.Group className="mb-3" controlId='salaryScale'>
+            <Form.Group className="mb-3" controlId="salaryScale">
               <Form.Label>Hệ số lương</Form.Label>
-              <Form.Control type="text" name="salaryScale" onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="text"
+                name="salaryScale"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId='annualLeave'>
+            <Form.Group className="mb-3" controlId="annualLeave">
               <Form.Label>Số ngày nghỉ còn lại</Form.Label>
-              <Form.Control type="text" name="annualLeave" onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="text"
+                name="annualLeave"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId='overTime'>
+            <Form.Group className="mb-3" controlId="overTime">
               <Form.Label>Số ngày đã làm thêm</Form.Label>
-              <Form.Control type="text" name="overTime" onChange={(event) => handleInputChange(event)} />
+              <Form.Control
+                type="text"
+                name="overTime"
+                onChange={(event) => handleInputChange(event)}
+              />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={handleClose}>
               Thêm
             </Button>
           </Form>
         </Modal.Body>
       </Modal>
     </>
-  )
+  );
 }
 
 export default AddStaff;
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,37 +1,52 @@
-import { Container } from 'react-bootstrap';
-import HomeBreadcrumb from './HomeBreadcrumbComponent';
-import { StaffBarChart, DeptsBudgetPieChart } from './ChartComponent';
-import AddStaff from './AddStaffComponent';
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import HomeBreadcrumb from "./HomeBreadcrumbComponent";
+import { StaffBarChart, DeptsBudgetPieChart } from "./ChartComponent";
+import AddStaff from "./AddStaffComponent";
+import { STAFFS, DEPARTMENTS } from "../shared/staffs";
+import { Button } from "bootstrap";
 
 /**
  * Sử dụng useParams để đọc giá trị từ URL (https://reactrouter.com/docs/en/v6/getting-started/tutorial#reading-url-params)
  */
 
 function Test() {
+  const localStaffs = JSON.parse(localStorage.getItem("staffs"));
+
+  const [staffs, setStaffs] = useState(localStaffs || []);
 
   return (
     <>
       <Container>
-        <HomeBreadcrumb links={[
-          { to: "/staffs", name: "Staffs" },
-          { to: "/departments", name: "Departments" },
-          { to: "/salaries", name: "Salaries" },
-        ]}
-          active="Test Page" />
+        <HomeBreadcrumb
+          links={[
+            { to: "/staffs", name: "Staffs" },
+            { to: "/departments", name: "Departments" },
+            { to: "/salaries", name: "Salaries" },
+          ]}
+          active="Test Page"
+        />
 
         <h1>Test Component</h1>
-        <AddStaff />
+
+        <AddStaff currentStaffList={STAFFS} />
+        <button onClick={() => window.location.reload()}>Refresh</button>
         <div className="row">
-          <div className='col-12 col-sm-6 p-1'>
+          {staffs.map((staff) => (
+            <p key={staff.id}>{JSON.stringify(staff)}</p>
+          ))}
+        </div>
+        <div className="row">
+          <div className="col-12 col-sm-6 p-1">
             <StaffBarChart />
           </div>
-          <div className='col-12 col-sm-6 p-1'>
+          <div className="col-12 col-sm-6 p-1">
             <DeptsBudgetPieChart />
           </div>
         </div>
       </Container>
     </>
-  )
+  );
 }
 
-export default Test
+export default Test;
