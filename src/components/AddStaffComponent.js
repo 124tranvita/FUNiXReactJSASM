634b4ec6e1de021b77037f022cb50e78646331b6
124tranvita <124tranvita@gmail.com>
2022-04-28 12:24:50 +0700

    Working on Validation

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import dateFormat from 'dateformat';
+
+let date = new Date();
+date = dateFormat(date, "yyyy-mm-dd")
 
 function AddStaff({ currentStaffList }) {
+
+  console.log(date);
+  //console.log(reformat);
+
   const storageStaffs = JSON.parse(localStorage.getItem("staffs"));
-  console.log(storageStaffs);
+  //console.log(storageStaffs);
 
   const [show, setShow] = useState(false);
   const [staffs, setStaffs] = useState(storageStaffs ?? []);
@@ -13,17 +21,27 @@ function AddStaff({ currentStaffList }) {
       : currentStaffList.length + storageStaffs.length
   );
 
-  console.log(staffId);
+  const [touched, setTouched] = useState({
+    name: false,
+    doB: false,
+    startDate: false
+  })
+
+  const [errors, setErrors] = useState({
+    name: "",
+    doB: "",
+    startDate: "",
+  })
 
   const [staff, setStaff] = useState({
     id: 0,
     name: "",
     doB: "",
-    salaryScale: "",
+    salaryScale: 0,
     startDate: "",
     department: "",
-    annualLeave: "",
-    overTime: "",
+    annualLeave: 0,
+    overTime: 0,
     image: "/assets/images/employee.svg",
   });
 
@@ -44,9 +62,63 @@ function AddStaff({ currentStaffList }) {
     });
   };
 
+  const handleBlur = (event) => {
+    const target = event.target;
+    const name = target.name;
+
+    setTouched({
+      ...touched,
+      [name]: true
+    })
+  }
+
+  //console.log(touched);
+
+  /*
+  const validate = (name, doB, startDate) => {
+    const errors = {
+      name: "",
+      doB: "",
+      startDate: "",
+    };
+
+    if (touched.name && name.length < 3)
+      errors.name = "Yêu cầu nhiều hơn 2 ký tự";
+    else if (touched.name && name.length > 15)
+      errors.name = "Yêu cầu dưới 15 ký tự";
+
+    if (touched.doB && !doB)
+      errors.doB = "Yêu cầu nhập ngày sinh";
+    else if (touched.doB && doB.slice(0, 4) >= date.slice(0, 4))
+      errors.doB = "Năm sinh lớn hơn năm hiện tại";
+
+
+    if (touched.startDate && !startDate)
+      errors.startDate = "Yêu cầu nhập ngày vào công ty";
+    else if (touched.startDate && startDate > date)
+      errors.startDate = "Ngày vào không được lớn hơn ngày hiện tại";
+
+    return errors;
+  }
+  
+
+  const errors = validate(staff.name, staff.doB, staff.startDate);
+  */
+
+  console.log(errors);
+
   const handleSubmit = (event) => {
+    if (staff.name === "") {
+      setErrors({
+        ...errors,
+        name: "Yêu cầu nhập"
+      })
+      event.preventDefault();
+    }
     //console.log("Current State is: " + JSON.stringify(staff));
     //alert("Current State is: " + JSON.stringify(staff));
+
+    //console.log('staff name: ' + staff.name);
 
     setStaffs((prevState) => {
       const newStaffs = [...prevState, { ...staff, id: staffId }];
@@ -56,7 +128,7 @@ function AddStaff({ currentStaffList }) {
       return newStaffs;
     });
 
-    //event.preventDefault();
+    event.preventDefault();
   };
 
   return (
@@ -77,24 +149,42 @@ function AddStaff({ currentStaffList }) {
                 type="text"
                 name="name"
                 placeholder="Họ và Tên"
+                isValid={errors.name === ""}
+                isInvalid={errors.name !== ""}
                 onChange={(event) => handleInputChange(event)}
+                onBlur={(event) => handleBlur(event)}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.name}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="doB">
               <Form.Label>Ngày sinh</Form.Label>
               <Form.Control
                 type="date"
                 name="doB"
+                isValid={errors.doB === ""}
+                isInvalid={errors.doB !== ""}
                 onChange={(event) => handleInputChange(event)}
+                onBlur={(event) => handleBlur(event)}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.doB}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="startDate">
               <Form.Label>Ngày vào công ty</Form.Label>
               <Form.Control
                 type="date"
                 name="startDate"
+                isValid={errors.startDate === ""}
+                isInvalid={errors.startDate !== ""}
                 onChange={(event) => handleInputChange(event)}
+                onBlur={(event) => handleBlur(event)}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.startDate}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="department">
               <Form.Label>Phòng ban</Form.Label>
@@ -115,6 +205,7 @@ function AddStaff({ currentStaffList }) {
               <Form.Control
                 type="text"
                 name="salaryScale"
+                value={staff.salaryScale}
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
@@ -123,6 +214,7 @@ function AddStaff({ currentStaffList }) {
               <Form.Control
                 type="text"
                 name="annualLeave"
+                value={staff.annualLeave}
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
@@ -131,10 +223,11 @@ function AddStaff({ currentStaffList }) {
               <Form.Control
                 type="text"
                 name="overTime"
+                value={staff.overTime}
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleClose}>
+            <Button variant="primary" type="submit">
               Thêm
             </Button>
           </Form>
