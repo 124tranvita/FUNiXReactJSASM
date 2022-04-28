e1ba998b4f7a098902ab92db27383335fd55be5d
124tranvita <124tranvita@gmail.com>
2022-04-28 16:02:16 +0700

    Added validation for Add Staff Form.

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -6,33 +6,50 @@ let date = new Date();
 date = dateFormat(date, "yyyy-mm-dd")
 
 function AddStaff({ currentStaffList }) {
-
-  console.log(date);
-  //console.log(reformat);
-
+  // Get the staff list from the localStorage
   const storageStaffs = JSON.parse(localStorage.getItem("staffs"));
-  //console.log(storageStaffs);
 
+  /** -----STATE AREA----- */
+  // Show/Unshow modal state
   const [show, setShow] = useState(false);
+
+  // localStorage staff list state (if localStorage === null => use empty array [])
   const [staffs, setStaffs] = useState(storageStaffs ?? []);
+
+  /**
+   * Set Staff's ID state.
+   * The new Id will contiune from the current Staff List
+   * If localStorage already stored an users, new ID will increase by sum of {currentStaffList} and {storageStaffs}
+   */
   const [staffId, setStaffId] = useState(
     storageStaffs === null
       ? currentStaffList.length
       : currentStaffList.length + storageStaffs.length
   );
 
+  /**
+   * Touched state, use to monitor the input (when user focus/click on input, this input is touched)
+   * Only name, doB, and startDate will be validated so only those field will be monitored
+   */
   const [touched, setTouched] = useState({
     name: false,
     doB: false,
     startDate: false
   })
 
-  const [errors, setErrors] = useState({
+  /**
+   * Required state, use to monitor if user already input the infomation or not when submit form
+   * If user submit form without entered valid data, error message will be displayed
+   */
+  const [required, setRequired] = useState({
     name: "",
     doB: "",
     startDate: "",
-  })
+  });
 
+  /**
+   * Add new staff state
+   */
   const [staff, setStaff] = useState({
     id: 0,
     name: "",
@@ -44,17 +61,29 @@ function AddStaff({ currentStaffList }) {
     overTime: 0,
     image: "/assets/images/employee.svg",
   });
+  /** -----STATE AREA----- */
 
+  /** -----HANDLER AREA----- */
   // Show/Close Modal handler
   const handleShow = () => setShow(true);
   const handleClose = () => {
     setShow(false);
   };
 
+  /**
+   * Input Handler
+   * Change the required state: When user is input, required will be set to '' (it mean user is already input and required doesn't make sense anymore)
+   * Set new state for Staff: Add new state for the staff (add new starff)
+   */
   const handleInputChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
+
+    setRequired(prevState => ({
+      ...prevState,
+      [name]: ''
+    }))
 
     setStaff({
       ...staff,
@@ -62,6 +91,11 @@ function AddStaff({ currentStaffList }) {
     });
   };
 
+  /**
+   * Blur Handler
+   * Change the touched state: When user focus and blur from the input, it mean user already touched into field
+   * Set touched to {True} for touched field
+   */
   const handleBlur = (event) => {
     const target = event.target;
     const name = target.name;
@@ -72,9 +106,42 @@ function AddStaff({ currentStaffList }) {
     })
   }
 
-  //console.log(touched);
+  /**
+   * Submit Handler. When user submit form:
+   * Check touched status and show "Yeu cau nhap" for the field that not touched yet. (1)
+   * If have any field that not touched yet -> prevent form to submit and end function by use return (2)
+   */
+  const handleSubmit = (event) => {
 
-  /*
+    // (1)
+    for (const property in touched) {
+      if (!touched[property]) {
+        setRequired(prevState => ({
+          ...prevState,
+          [property]: 'Yêu cầu nhập'
+        }))
+      }
+    }
+
+    // (2)
+    if (!touched.name || !touched.doB || !touched.startDate) {
+      event.preventDefault();
+      return;
+    }
+
+    // Add new staffs into new staff list (that saved on localStrage) - include add staff's id
+    setStaffs((prevState) => {
+      const newStaffs = [...prevState, { ...staff, id: staffId }];
+
+      // save new staff list to localStorage
+      localStorage.setItem("staffs", JSON.stringify(newStaffs));
+
+      return newStaffs;
+    });
+  };
+  /** -----HANDLER AREA----- */
+
+  // Validate input function
   const validate = (name, doB, startDate) => {
     const errors = {
       name: "",
@@ -100,36 +167,9 @@ function AddStaff({ currentStaffList }) {
 
     return errors;
   }
-  
 
+  // Call validate function
   const errors = validate(staff.name, staff.doB, staff.startDate);
-  */
-
-  console.log(errors);
-
-  const handleSubmit = (event) => {
-    if (staff.name === "") {
-      setErrors({
-        ...errors,
-        name: "Yêu cầu nhập"
-      })
-      event.preventDefault();
-    }
-    //console.log("Current State is: " + JSON.stringify(staff));
-    //alert("Current State is: " + JSON.stringify(staff));
-
-    //console.log('staff name: ' + staff.name);
-
-    setStaffs((prevState) => {
-      const newStaffs = [...prevState, { ...staff, id: staffId }];
-
-      localStorage.setItem("staffs", JSON.stringify(newStaffs));
-
-      return newStaffs;
-    });
-
-    event.preventDefault();
-  };
 
   return (
     <>
@@ -143,49 +183,59 @@ function AddStaff({ currentStaffList }) {
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={(event) => handleSubmit(event)}>
+            {/* name */}
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Tên</Form.Label>
               <Form.Control
                 type="text"
                 name="name"
                 placeholder="Họ và Tên"
-                isValid={errors.name === ""}
-                isInvalid={errors.name !== ""}
+                isValid={touched.name && errors.name === ""}
+                isInvalid={errors.name !== "" || required.name !== ""}
                 onChange={(event) => handleInputChange(event)}
                 onBlur={(event) => handleBlur(event)}
               />
               <Form.Control.Feedback type="invalid">
-                {errors.name}
+                {errors.name || required.name}
               </Form.Control.Feedback>
             </Form.Group>
+            {/* name */}
+
+            {/* doB */}
             <Form.Group className="mb-3" controlId="doB">
               <Form.Label>Ngày sinh</Form.Label>
               <Form.Control
                 type="date"
                 name="doB"
-                isValid={errors.doB === ""}
-                isInvalid={errors.doB !== ""}
+                isValid={touched.doB && errors.doB === ""}
+                isInvalid={errors.doB !== "" || required.doB}
                 onChange={(event) => handleInputChange(event)}
                 onBlur={(event) => handleBlur(event)}
               />
               <Form.Control.Feedback type="invalid">
-                {errors.doB}
+                {errors.doB || required.doB}
               </Form.Control.Feedback>
             </Form.Group>
+            {/* doB */}
+
+            {/* startDate */}
             <Form.Group className="mb-3" controlId="startDate">
               <Form.Label>Ngày vào công ty</Form.Label>
               <Form.Control
                 type="date"
                 name="startDate"
-                isValid={errors.startDate === ""}
-                isInvalid={errors.startDate !== ""}
+                isValid={touched.startDate && errors.startDate === ""}
+                isInvalid={errors.startDate !== "" || required.startDate !== ""}
                 onChange={(event) => handleInputChange(event)}
                 onBlur={(event) => handleBlur(event)}
               />
               <Form.Control.Feedback type="invalid">
-                {errors.startDate}
+                {errors.startDate || required.startDate}
               </Form.Control.Feedback>
             </Form.Group>
+            {/* startDate */}
+
+            {/* department */}
             <Form.Group className="mb-3" controlId="department">
               <Form.Label>Phòng ban</Form.Label>
               <Form.Select
@@ -200,6 +250,9 @@ function AddStaff({ currentStaffList }) {
                 <option value="5">Finance</option>
               </Form.Select>
             </Form.Group>
+            {/* department */}
+
+            {/* salaryScale */}
             <Form.Group className="mb-3" controlId="salaryScale">
               <Form.Label>Hệ số lương</Form.Label>
               <Form.Control
@@ -209,6 +262,9 @@ function AddStaff({ currentStaffList }) {
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
+            {/* salaryScale */}
+
+            {/* annualLeave */}
             <Form.Group className="mb-3" controlId="annualLeave">
               <Form.Label>Số ngày nghỉ còn lại</Form.Label>
               <Form.Control
@@ -218,6 +274,9 @@ function AddStaff({ currentStaffList }) {
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
+            {/* annualLeave */}
+
+            {/* overTime */}
             <Form.Group className="mb-3" controlId="overTime">
               <Form.Label>Số ngày đã làm thêm</Form.Label>
               <Form.Control
@@ -227,6 +286,7 @@ function AddStaff({ currentStaffList }) {
                 onChange={(event) => handleInputChange(event)}
               />
             </Form.Group>
+            {/* overTime */}
             <Button variant="primary" type="submit">
               Thêm
             </Button>
